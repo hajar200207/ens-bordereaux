435c1e17773c5829463c8c487189602590b67ed5
hajar200207 <ouadihajar1234@gmail.com>
2026-03-11 22:48:55 +0100

    first commit

---
+++ ens-bordereaux/app/Http/Controllers/DocumentCommentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DocumentComment;
+use App\Http\Requests\StoreDocumentCommentRequest;
+use App\Http\Requests\UpdateDocumentCommentRequest;
+
+class DocumentCommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDocumentCommentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DocumentComment $documentComment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DocumentComment $documentComment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDocumentCommentRequest $request, DocumentComment $documentComment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DocumentComment $documentComment)
+    {
+        //
+    }
+}
---
+++ ens-bordereaux/app/Models/DocumentAssignment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DocumentAssignment extends Model
+{
+    /** @use HasFactory<\Database\Factories\DocumentAssignmentFactory> */
+    use HasFactory;
+}
